34ae74d9704c83f9d58c78c0178fac4b4dc23b00
matatsuna <ev50510@meiji.ac.jp>
2018-08-16 16:13:05 +0900

    Initial commit

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
-SLACKTOKEN = 'xxxx-xxxxxxxxx-xxxx'
-BOTID = '*********'
-          
+SLACKTOKEN = 'xoxb-215164122018-hi7hsBqllLm0X0cPbncpxJy8'
+BOTID = 'U6B4U3L0J'
---
+++ secret_config.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+SLACKTOKEN = 'xxxx-xxxxxxxxx-xxxx'
+BOTID = '*********'
+          
